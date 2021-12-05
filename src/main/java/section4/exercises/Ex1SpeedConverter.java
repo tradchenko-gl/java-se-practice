301570ef605f02f71c2c8642e96be21643ccd6cf
Tetiana Radchenko <95315545+tradchenko-gl@users.noreply.github.com>
2021-12-05 18:55:17 +0100

    exercise 1

--- src/main/java/section4/exercises/Ex1SpeedConverter.java
+++ src/main/java/section4/exercises/Ex1SpeedConverter.java
@@ -3,10 +3,12 @@ package section4.exercises;
 public class Ex1SpeedConverter {
 
     public static void main(String[] args) {
-        double kilomerters = 1.5;
+        double kilomerters = -9;
 
         long milesPerHour = toMilesPerHour(kilomerters);
         System.out.println(milesPerHour);
+
+        printConversion(kilomerters);
 
     }
 
@@ -14,10 +16,19 @@ public class Ex1SpeedConverter {
         long milesPerHour = -1;
         double milesInOneKm = 1.609;
 
-        if (kilometersPerHour > 0) {
-            milesPerHour = Math.round(kilometersPerHour*milesInOneKm);
+        if (kilometersPerHour >= 0) {
+            milesPerHour = Math.round(kilometersPerHour/milesInOneKm);
         }
 
         return milesPerHour;
+    }
+
+    public static void printConversion (double kilometersPerHour) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+
+        if (kilometersPerHour >= 0) {
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        } else
+            System.out.println("Invalid Value");
     }
 }
