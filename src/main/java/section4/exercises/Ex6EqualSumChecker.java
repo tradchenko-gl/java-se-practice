e0524a9c44ba1e902e5c290ccde4705de280e8d7
Tetiana Radchenko <95315545+tradchenko-gl@users.noreply.github.com>
2021-12-05 21:52:54 +0100

    exercise 6

---
+++ src/main/java/section4/exercises/Ex6EqualSumChecker.java
@@ -0,0 +1,14 @@
+package section4.exercises;
+
+public class Ex6EqualSumChecker {
+
+    public static boolean hasEqualSum (int number1, int number2, int number3) {
+        boolean isSumEqual = false;
+        int sum = (number1 + number2);
+
+        if (sum == number3) {
+            isSumEqual = true;
+        }
+        return isSumEqual;
+    }
+}
