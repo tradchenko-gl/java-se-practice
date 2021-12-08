d664080e8976bd0bbec8ab7f30b297ff946d73b1
Tetiana Radchenko <95315545+tradchenko-gl@users.noreply.github.com>
2021-12-08 20:10:34 +0100

    module 5, challenge 2, 3

---
+++ src/main/java/section5/challenges/Ch2ForLoopChallenge.java
@@ -0,0 +1,18 @@
+package section5.challenges;
+
+public class Ch2ForLoopChallenge {
+
+    public static void main(String[] args) {
+        for (int i=8; i>1; i--) {
+            System.out.println("10000 at " + i + " interest = " + calculateInterest(10000, i));
+        }
+
+
+    }
+
+
+
+    public static double calculateInterest (double amount, double interestRate) {
+        return (amount*(interestRate/100));
+    }
+}
