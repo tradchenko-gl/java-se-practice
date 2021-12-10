cbfc4e0be1ebd3f134a48d011b6d8099db97cd9a
Tetiana Radchenko <tetiana.radchenko@globallogic.com>
2021-12-10 13:33:20 +0100

    m5 ex 18-20

---
+++ src/main/java/section5/exersises/Ex19LastDigitChecker.java
@@ -0,0 +1,29 @@
+package section5.exersises;
+
+public class Ex19LastDigitChecker {
+
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(23, 32, 42));
+    }
+
+    public static boolean hasSameLastDigit(int number1, int number2, int number3) {
+
+        if ((isValid(number1)) && (isValid(number2)) && (isValid(number3))) {
+            int lastDigit1 = number1 % 10;
+            int lastDigit2 = number2 % 10;
+            int lastDigit3 = number3 % 10;
+
+            if ((lastDigit1 == lastDigit2) || (lastDigit1 == lastDigit3) || (lastDigit3 == lastDigit2)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean isValid (int number) {
+        if ((number >= 10) && (number <= 1000)) {
+            return true;
+        } else return false;
+    }
+}
---
+++ src/main/java/section5/exersises/Ex20GreatestCommonDivisor.java
@@ -0,0 +1,16 @@
+package section5.exersises;
+
+public class Ex20GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor (int first, int second) {
+
+        if ((first >= 10) && (second >= 10)) {
+            int minValue = Math.min(first, second);
+            for (int i = minValue; i > 0; i--) {
+                if ((first % i == 0) && (second % i == 0)) {
+                    return i;
+                }
+            }
+        } return -1;
+    }
+}
