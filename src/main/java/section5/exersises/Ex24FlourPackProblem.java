f46184716d3eed813346894ce93dd62d19c17c61
Tetiana Radchenko <95315545+tradchenko-gl@users.noreply.github.com>
2021-12-12 21:15:37 +0100

    m5 Ex 24 - 24

---
+++ src/main/java/section5/exersises/Ex24FlourPackProblem.java
@@ -0,0 +1,21 @@
+package section5.exersises;
+
+public class Ex24FlourPackProblem {
+
+    public static boolean canPack (int bigCount, int smallCount, int goal) {
+        //bigCount is a number of bags 5 kg each
+        //smallCount is a number of bags 1 kg each
+        //goal = number of kg to assemble
+        if ((bigCount >=0) && (smallCount >= 0) && (goal >= 0)) {
+
+            for (int i = 0; i <= bigCount; i++) {
+                for (int j = 0; j <= smallCount; j++) {
+                    if (goal == (i*5) + j) {
+                       return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/section5/exersises/Ex25LargestPrime_Solution2.java
@@ -0,0 +1,34 @@
+package section5.exersises;
+
+public class Ex25LargestPrime_Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(217));
+    }
+
+    public static int getLargestPrime (int number) {
+
+        if (number > 1) {
+            for (int i = number; i > 1; i--) {
+                if ((number % i == 0) && (isPrimeNumber(i))) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static boolean isPrimeNumber (int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
