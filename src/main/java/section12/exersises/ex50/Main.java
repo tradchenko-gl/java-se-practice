d825046d10c80eafb8687edd635ae55d8884f901
Tetiana Radchenko <95315545+tradchenko-gl@users.noreply.github.com>
2022-01-22 21:57:38 +0100

    s12 ex50

--- src/main/java/section12/exersises/ex50/Main.java
+++ src/main/java/section12/exersises/ex50/Main.java
@@ -1,4 +1,4 @@
-package section12.challenges.adventureGame;
+package section12.exersises.ex50;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,9 +6,9 @@ import java.util.Scanner;
 
 public class Main {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private Map<String, String> vocabulary = new HashMap<String, String>();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
         locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
@@ -32,6 +32,16 @@ public class Main {
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
 
+    }
+
+    public void command() {
+        Scanner scanner = new Scanner(System.in);
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("QUIT", "Q");
+
         int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDescription());
@@ -46,47 +56,22 @@ public class Main {
             }
             System.out.println();
 
-            String direction = null;
-            String userCommand = scanner.nextLine().toUpperCase();
-            if (userCommand.length() > 1) {
-                String[] commandStrings = userCommand.split(" ");
+            String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] commandStrings = direction.split(" ");
                 for(String commandString : commandStrings) {
-                    switch (commandString) {
-                        case "WEST": {
-                            direction = "W";
-                            break;
-                        }
-                        case "NORTH": {
-                            direction = "N";
-                            break;
-                        }
-                        case "EAST": {
-                            direction = "E";
-                            break;
-                        }
-                        case "SOUTH": {
-                            direction = "S";
-                            break;
-                        }
-                        case "QUIT": {
-                            direction = "Q";
-                            break;
-                        }
-                    }
-                    if(direction != null) {
+                    if(vocabulary.containsKey(commandString)) {
+                        direction = vocabulary.get(commandString);
                         break;
                     }
                 }
+            }
+            if(exits.containsKey(direction)) {
+                loc = exits.get(direction);
 
             } else {
-                direction = userCommand;
+                System.out.println("You cannot go in that direction");
             }
-                if(exits.containsKey(direction)) {
-                    loc = exits.get(direction);
-
-                } else {
-                    System.out.println("You cannot go in that direction");
-                }
         }
     }
 }
